af9b694abeeab2ec87df21a562d7a4e628a7c030
Mars <mallorysmith6464@gmail.com>
2019-11-04 13:17:20 -0500

    restructured folders, added num to string code kata solution in python

---
+++ public/Javascript/SurfaceAreaAndVolume.js
@@ -0,0 +1,17 @@
+// Level: 8kyu
+// Name: Surface Area And Volume of A Box
+// Instructions: Write a function that returns the total surface area and volume of a box as an array: [area, volume]
+
+//Solution in Javascript
+const getSize = (width, height, depth) => {
+  const area = (width * depth + depth * height + width * height) * 2
+  const volume = width * height * depth
+  return [area, volume]
+}
+
+// Sample Tests Passed:
+// Test.assert_equals(get_size(4, 2, 6), [88,48])
+// Test.assert_equals(get_size(1, 1, 1), [6,1])
+// Test.assert_equals(get_size(1, 2, 1), [10,2])
+// Test.assert_equals(get_size(1, 2, 2), [16,4])
+// Test.assert_equals(get_size(10, 10, 10), [600,1000])
---
+++ public/Python/NumToString.py
@@ -0,0 +1,11 @@
+# Level: 8kyu
+# Name: Convert a Number to a String!
+# Instructions: Transform a number into a string
+
+
+# solution in python
+def number_to_string(num):
+    return str(num)
+
+# sample tests passed:
+# test.expect(number_to_string(67) == '67');
