a02431b7287e27f00145a011229224fc133b3b21
Mars <mallorysmith6464@gmail.com>
2019-11-06 20:11:51 -0500

    added number to string javascript solution

---
+++ Javascript/NumToString.js
@@ -0,0 +1,11 @@
+// # Level: 8kyu
+// # Name: Convert a Number to a String!
+// # Instructions: Transform a number into a string
+
+function numberToString(num) {
+  let x = String(num)
+  return x
+}
+
+// # sample tests passed:
+// # test.expect(number_to_string(67) == '67');
