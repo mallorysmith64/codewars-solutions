211d91fa5ce63b4ba75ac85501fcbf981af276cd
Mars <mallorysmith6464@gmail.com>
2019-11-06 20:03:26 -0500

    added string to number python solution

---
+++ Python/StringToNum.py
@@ -0,0 +1,15 @@
+# Level: 8kyu
+# Name: Convert a String to a Number!
+# Instructions: Transform a string into a number
+
+# solution in python
+
+
+def string_to_number(s):
+    return int(s)
+
+# sample tests passed:
+# test.assert_equals( string_to_number("1234"), 1234)
+# test.assert_equals( string_to_number("605"), 605)
+# test.assert_equals( string_to_number("1405"), 1405)
+# test.assert_equals( string_to_number("1234"), 1234)
