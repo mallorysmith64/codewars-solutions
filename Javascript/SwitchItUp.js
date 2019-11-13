7c7a4e86247c102cafda98457527158b307e8db8
mallorysmith64 <mallorysmith64@gmail.com>
2019-11-12 22:02:31 -0500

    added switch it up solution in javascript

---
+++ Javascript/SwitchItUp.js
@@ -0,0 +1,36 @@
+// Level: 8kyu
+// Name: Switch it Up!
+// Instructions: When provided with a number between 0-9, return it in words.
+// Input :: 1 Output :: "One"
+// If your language supports it, try using a switch statement.
+
+//Solution in Javascript
+const switchItUp = number => {
+  switch (number) {
+    case 0:
+      return "Zero";
+    case 1:
+      return "One";
+    case 2:
+      return "Two";
+    case 3:
+      return "Three";
+    case 4:
+      return "Four";
+    case 5:
+      return "Five";
+    case 6:
+      return "Six";
+    case 7:
+      return "Seven";
+    case 8:
+      return "Eight";
+    case 9:
+      return "Nine";
+  }
+};
+
+// Sample Tests Passed:
+// Test.assertEquals(switchItUp(1),"One");
+// Test.assertEquals(switchItUp(3),"Three");
+// Test.assertEquals(switchItUp(5),"Five");
