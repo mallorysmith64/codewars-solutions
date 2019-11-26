4826357c6b3848f1066a1fea5e9c12ee8f57f816
mallorysmith64 <mallorysmith64@gmail.com>
2019-11-25 19:53:02 -0500

    added apple turnover solution in javascript

---
+++ Javascript/AlanPartridgeII_AppleTurnover.js
@@ -0,0 +1,21 @@
+// Level: 8kyu
+// Name: Alan Partridge II - Apple Turnover
+// Instructions: Alan is known for referring to the temperature of the apple turnover as 'Hotter than the sun!'. According to space.com the temperature of the sun's corona is 2,000,000 degrees C, but we will ignore the science for now.
+
+// Your job is simple, if (x) squared is more than 1000, return 'It's hotter than the sun!!', else, return 'Help yourself to a honeycomb Yorkie for the glovebox.'.
+// X will be either a number or a string. Both are valid.
+
+// Solution in Javascript
+function apple(x){
+    if (x**2 > 1000) {
+    return "It's hotter than the sun!!"
+    } else {
+    return "Help yourself to a honeycomb Yorkie for the glovebox."
+    }
+  }  
+  
+// Sample Tests Passed:
+// Test.describe("Example tests",_=>{
+// Test.assertEquals(apple('50'), 'It\'s hotter than the sun!!'); 
+// Test.assertEquals(apple(4), 'Help yourself to a honeycomb Yorkie for the glovebox.'); 
+// });
