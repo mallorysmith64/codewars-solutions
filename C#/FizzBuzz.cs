f95c69927b260dc9864fb1108f72b152de3c485b
mallorysmith64 <mallorysmith64@gmail.com>
2019-12-01 18:59:41 -0500

    saving fizzbuzz progress

--- C#/FizzBuzz.cs
+++ C#/FizzBuzz.cs
@@ -1,10 +1,11 @@
 using System;
+using System.Linq;
 
 public class FizzBuzz
 {
     public static string[] GetFizzBuzzArray(int n)
     {
-        //todo
+        if (n<=0) throw new ArgumentOutOfRangeException();
+        return Enumerable.Range(1, n).Select(x => x % 15 == 0 ? "FizzBuzz" : x % 3 == 0 ? "Fizz" : x % 5 == 0 ? "Buzz" : x.ToString()).ToArray();
     }
-
 }
