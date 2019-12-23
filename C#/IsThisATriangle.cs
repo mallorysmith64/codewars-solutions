35329a6808be582f5ee17c6d1803ef7fb27b95ad
mallorysmith64 <mallorysmith64@gmail.com>
2019-12-23 08:04:55 -0500

    added is this a triangle solution in C#, helpful hints to solve the kata, and added an image to help explain the triangle inequality theorem

---
+++ C#/IsThisATriangle.cs
@@ -0,0 +1,44 @@
+// Level: 7kyu
+// Name: Is this a triangle?
+// Instructions: Implement a method that accepts 3 integer values a, b, c. 
+//The method should return true if a triangle can be built with the sides of given length and false in any other case.
+// (In this case, all triangles must have surface greater than 0 to be accepted).
+
+// Solution 1 in C#:
+//step 1: Know the triangle inequality theorem
+//Triangle Inequality Theorem: The sum of the lengths of any two sides of a triangle > the length of the third side.
+//step 2: return this theorem by either adding the sides together and setting them greater than the third side or
+// subtract two sides and set them less than the third side
+
+public class Triangle
+{
+    public static bool IsTriangle(int a, int b, int c)
+    {
+        return a + b > c && b + c > a && c + a > b;
+    }
+}
+
+// Solution 2:
+// using System;
+
+// public class Triangle
+// {
+//     public static bool IsTriangle(int a, int b, int c)
+//     {
+//        return a - b < c && b - c <a && c - a < b;
+//     }
+// }
+
+// Sample Tests Passed:
+// using NUnit.Framework;
+// using System;
+
+// [TestFixture]
+// public class TriangleTests
+// {
+//     [Test]
+//     public void IsTriangle_ValidPostiveNumbers_ReturnsTrue()
+//     {
+//         Assert.IsTrue(Triangle.IsTriangle(5, 7, 10));
+//     }
+// }
