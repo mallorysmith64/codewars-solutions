89c9834020fba09d0b2c01134111cab3071ca42a
mallorysmith64 <mallorysmith64@gmail.com>
2019-12-28 15:46:33 -0500

    provided makeUpperCase solution in C#

--- C#/MakeUpperCase.cs
+++ C#/MakeUpperCase.cs
@@ -3,20 +3,27 @@
 // Instructions: Write function makeUpperCase
 
 // Solution in C#
+public class Kata
+  {
+        public static string MakeUpperCase(string str)
+        {
+             return str.ToUpper();
+        }
 
-//todo write solution here
+  }
+
 
 // Sample Tests Passed:
-namespace Learning {
-  using NUnit.Framework;
-  using System;
-  [TestFixture]
-  public class MakeUpperCaseTest
-  {
-    [Test]
-    public void BasicTest()
-    {
-       Assert.AreEqual("HELLO", Kata.MakeUpperCase("hello"));    
-    }
-  }    
-}
+// namespace Learning {
+//   using NUnit.Framework;
+//   using System;
+//   [TestFixture]
+//   public class MakeUpperCaseTest
+//   {
+//     [Test]
+//     public void BasicTest()
+//     {
+//        Assert.AreEqual("HELLO", Kata.MakeUpperCase("hello"));    
+//     }
+//   }    
+// }
