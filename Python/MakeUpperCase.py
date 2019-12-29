a58f8a095ceedf236da4554a47659cba88dc2fe4
mallorysmith64 <mallorysmith64@gmail.com>
2019-12-29 11:53:24 -0500

    added MakeUpperCase solution in python, one step to help others solve this problem, one resource, and a sample test case

---
+++ Python/MakeUpperCase.py
@@ -0,0 +1,12 @@
+# Level: 8kyu
+# Name: MakeUpperCase
+# Instructions: Write function makeUpperCase
+
+# Solution in Python
+#step1: know string upper() method
+#resource: https://www.programiz.com/python-programming/methods/string/upper
+def make_upper_case(s):
+   return s.upper()
+
+# Sample Tests Passed:
+# test.assert_equals(make_upper_case("hello"), "HELLO")
