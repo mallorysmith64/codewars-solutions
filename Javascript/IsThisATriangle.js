210c3255854b30d906e848ac2e52f155f4f37cb4
mallorysmith64 <mallorysmith64@gmail.com>
2019-12-31 13:45:33 -0500

    corrected IsThisATriangle JS solution, also added a 2nd solution

--- Javascript/IsThisATriangle.js
+++ Javascript/IsThisATriangle.js
@@ -4,14 +4,17 @@
 // The method should return true if a triangle can be built with the sides of given length and false in any other case.
 // (In this case, all triangles must have surface greater than 0 to be accepted).
 
-// Solution in Javascript
-function hoopCount (n) {
-    if (n >= 10) {
-    return "Great, now move on to tricks";
-    } else {
-    return "Keep at it until you get it";
-     }
-  }
+// Solution in Javascript:
+function isTriangle(a,b,c)
+{
+   return a + b > c && b + c > a && c + a > b
+}
+
+// Solution 2:
+function isTriangle(a,b,c)
+{
+   return a - b < c && b - c <a && c - a < b;
+}
 
 // Sample Tests Passed:
 // Test.describe("PublicTest", function() {
