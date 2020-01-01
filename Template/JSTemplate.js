540bd0d19358827c9301c712acee1da0718ebff6
mallorysmith64 <mallorysmith64@gmail.com>
2020-01-01 18:48:06 -0500

    added problem/solution templates for C#, JS, and python to make formatting easier

---
+++ Template/JSTemplate.js
@@ -0,0 +1,12 @@
+// Name of Problem: 
+// Language: Javascript
+// Level: 
+// Instructions:
+
+// Solution 1:
+  
+
+// Solution 2:
+
+
+// Sample Tests Passed:
---
+++ Template/PyTemplate.py
@@ -0,0 +1,12 @@
+# Name of Problem: 
+# Language: Python
+# Level: 
+# Instructions:
+
+# Solution 1:
+  
+
+# Solution 2:
+
+
+# Sample Tests Passed:
