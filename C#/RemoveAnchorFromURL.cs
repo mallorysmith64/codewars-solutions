bbd37c8df19d5831e03e43f6a72c7d2edd51ae86
mallorysmith64 <mallorysmith64@gmail.com>
2020-01-01 18:37:56 -0500

    added RemoveAnchorFromURL C# solution and sample test cases passed

---
+++ C#/RemoveAnchorFromURL.cs
@@ -0,0 +1,35 @@
+// Level: 7kyu
+// Name: Remove anchor from URL
+// Instructions: Complete the function/method so that it returns the url with anything after the anchor (#) removed.
+
+// Examples:
+// Kata.RemoveUrlAnchor("www.codewars.com#about") => "www.codewars.com"
+// Kata.RemoveUrlAnchor("www.codewars.com?page=1") => "www.codewars.com?page=1"
+
+// Solution in C#:
+public static class Kata
+{
+  public static string RemoveUrlAnchor(string url)
+  {
+    return url.Split("#")[0];
+  }
+}
+
+// Sample Tests Passed:
+// namespace Solution 
+// {
+//   using NUnit.Framework;
+//   using System;
+
+//   [TestFixture]
+//   public class Tests
+//   {
+//     [Test, Description("Sample Tests")]
+//     public void SampleTests()
+//     {
+//       Assert.AreEqual("www.codewars.com", Kata.RemoveUrlAnchor("www.codewars.com#about"));
+//       Assert.AreEqual("www.codewars.com/katas/?page=1", Kata.RemoveUrlAnchor("www.codewars.com/katas/?page=1#about"));
+//       Assert.AreEqual("www.codewars.com/katas/", Kata.RemoveUrlAnchor("www.codewars.com/katas/"));
+//     }
+//   }
+// }
