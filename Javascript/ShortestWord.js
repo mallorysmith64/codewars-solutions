1788cab11920bc13a217582c938d9484e7dba040
mallorysmith64 <mallorysmith64@gmail.com>
2020-01-07 19:55:37 -0500

    added 2 additional JS solutions for shortestword problem

--- Javascript/ShortestWord.js
+++ Javascript/ShortestWord.js
@@ -5,7 +5,24 @@
 // Instructions: Simple, given a string of words, return the length of the shortest word(s).
 // String will never be empty and you do not need to account for different data types.
 
-// Solution:
+// Solution 1 using split(), sort(), and pop():
+function findShort(s){
+  return s.split(' ').sort((a,b) => (b.length - a.length)).pop().length
+}
+
+// Solution 2:
+function findShort(s){
+  let arr = s.split(' ')
+  let shortest = arr[0]
+    for (let i = 0; i < arr.length; i++) {
+    if (arr[i].length < shortest.length) {
+  shortest = arr[i]
+    }
+  }
+return shortest.length;
+}
+
+// Solution 3 using Infinity:
 const findShort = s => {
   let wordArr = s.split(' ')
   let shortest = Infinity
