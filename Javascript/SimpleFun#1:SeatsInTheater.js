6a1de29819cfef21be6816d9ae56a9f405d28810
mallorysmith64 <mallorysmith64@gmail.com>
2020-01-12 22:00:54 -0500

    reformatted test cases in seatsintheater problem

--- Javascript/SimpleFun#1:SeatsInTheater.js
+++ Javascript/SimpleFun#1:SeatsInTheater.js
@@ -44,16 +44,10 @@ function seatsInTheater(nCols, nRows, col, row) {
 
 // Sample Tests Passed:
 // describe("Basic Tests", function(){
-//     it("It should works for basic tests.", function(){
-    
+// it("It should works for basic tests.", function(){
 //     Test.assertEquals(seatsInTheater(16,11,5,3) , 96)
-    
 //     Test.assertEquals(seatsInTheater(1,1,1,1) , 0)
-    
 //     Test.assertEquals(seatsInTheater(13,6,8,3) , 18)
-    
 //     Test.assertEquals(seatsInTheater(60,100,60,1) , 99)
-    
 //     Test.assertEquals(seatsInTheater(1000,1000,1000,1000) , 0)
-    
 //     })})
