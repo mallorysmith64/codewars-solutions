3c3e56f7d1be2e3bd684881df20cdf4a4302b6a6
mallorysmith64 <mallorysmith64@gmail.com>
2020-01-12 22:00:00 -0500

    addd/fixed some test cases seatsintheater problem passed

--- Javascript/SimpleFun#1:SeatsInTheater.js
+++ Javascript/SimpleFun#1:SeatsInTheater.js
@@ -43,27 +43,17 @@ function seatsInTheater(nCols, nRows, col, row) {
   }
 
 // Sample Tests Passed:
-// namespace Solution
-// {
-//   using NUnit.Framework;
-  
-//   using System;
-
-//   [TestFixture]
-//   public class Tests
-//   {
-//     [Test]
-//     public void ExampleTests()
-//     {
-//       Assert.AreEqual(96, Kata.SeatsInTheater(16, 11, 5, 3));
-
-//       Assert.AreEqual(0, Kata.SeatsInTheater(1, 1, 1, 1));
-      
-//       Assert.AreEqual(18, Kata.SeatsInTheater(13, 6, 8, 3));
-      
-//       Assert.AreEqual(99, Kata.SeatsInTheater(60,100,60,1));
-      
-//       Assert.AreEqual(0, Kata.SeatsInTheater(1000,1000,1000,1000));
-//     }
-//   }
-// }
+// describe("Basic Tests", function(){
+//     it("It should works for basic tests.", function(){
+    
+//     Test.assertEquals(seatsInTheater(16,11,5,3) , 96)
+    
+//     Test.assertEquals(seatsInTheater(1,1,1,1) , 0)
+    
+//     Test.assertEquals(seatsInTheater(13,6,8,3) , 18)
+    
+//     Test.assertEquals(seatsInTheater(60,100,60,1) , 99)
+    
+//     Test.assertEquals(seatsInTheater(1000,1000,1000,1000) , 0)
+    
+//     })})
