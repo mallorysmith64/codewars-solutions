86c6c53c54d771624d3f9634e0980e1dc4e57c30
mallorysmith64 <mallorysmith64@gmail.com>
2020-01-17 18:27:34 -0500

    added growing plant problem and solution in C# and some test cases passed

--- C#/Fizz-Buzz.cs
+++ C#/Fizz-Buzz.cs
@@ -2,7 +2,10 @@
 // Level: 7kyu
 // Name of Problem: Fizz / Buzz
 
-// Instructions: Write a function that takes an integer and returns an array [A, B, C], where A is the number of multiples of 3 (but not 5) below the given integer, B is the number of multiples of 5 (but not 3) below the given integer and C is the number of multiples of 3 and 5 below the given integer.
+// Instructions: Write a function that takes an integer and returns an array [A, B, C], 
+// A is the number of multiples of 3 (but not 5) below the given integer, 
+// B is the number of multiples of 5 (but not 3) below the given integer,
+// C is the number of multiples of 3 and 5 below the given integer
 
 // Examples: 
 // solution(20) should return [5, 2, 1]
