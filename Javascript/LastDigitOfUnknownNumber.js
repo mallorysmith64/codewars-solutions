8e32697c12fc81a5639e9f93b1d3f834467b6500
mallorysmith64 <mallorysmith64@gmail.com>
2025-08-14 04:08:18 -0400

    added problem, bigint

---
+++ Javascript/LastDigitOfUnknownNumber.js
@@ -0,0 +1,8 @@
+//Last digit of a large number
+
+function lastDigit(a, b) {
+    let dj = Math.abs(b % a); //positive only case
+    console.log(dj)
+    // let djdjd = dj.BigInt()
+    return dj
+}
